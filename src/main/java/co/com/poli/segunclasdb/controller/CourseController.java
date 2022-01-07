e290a87c763b4f1d3db5db8933d66b9970d6874d
Edwin Rivera <edriver11@elpoli.edu.co>
2022-01-07 14:07:00 -0500

    Agregando anotacion @RequestBody a las clases controller en los post para que pueda recibir los datos

--- src/main/java/co/com/poli/segunclasdb/controller/CourseController.java
+++ src/main/java/co/com/poli/segunclasdb/controller/CourseController.java
@@ -3,10 +3,7 @@ package co.com.poli.segunclasdb.controller;
 import co.com.poli.segunclasdb.entities.Course;
 import co.com.poli.segunclasdb.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,7 +20,7 @@ public class CourseController {
     }
 
     @PostMapping
-    public Course create(Course course){
+    public Course create(@RequestBody Course course){
         return service.create(course);
     }
 
--- src/main/java/co/com/poli/segunclasdb/controller/InstructorController.java
+++ src/main/java/co/com/poli/segunclasdb/controller/InstructorController.java
@@ -5,10 +5,7 @@ import co.com.poli.segunclasdb.entities.Instructor;
 import co.com.poli.segunclasdb.services.CourseService;
 import co.com.poli.segunclasdb.services.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class InstructorController {
      }
 
      @PostMapping
-     public Instructor create(Instructor instructor){
+     public Instructor create(@RequestBody Instructor instructor){
         return service.createInstructor(instructor);
      }
 
